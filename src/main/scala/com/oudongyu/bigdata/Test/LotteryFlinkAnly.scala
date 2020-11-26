30c7e20c0df1a106c78f94c7d7d9adec1e8c80bc
oudongyu <37198294+oudongyu@users.noreply.github.com>
2020-11-26 17:19:41 +0800

    Flink测试 LotteryFlinkAnly

--- src/main/scala/com/oudongyu/bigdata/Test/LotteryFlinkAnly.scala
+++ src/main/scala/com/oudongyu/bigdata/Test/LotteryFlinkAnly.scala
@@ -1,19 +1,24 @@
 package com.oudongyu.bigdata.Test
 
+import org.apache.flink.api.scala.ExecutionEnvironment
 import org.apache.flink.contrib.streaming.state.RocksDBStateBackend
 import org.apache.flink.streaming.api.scala._
 
 object LotteryFlinkAnly {
   def main(args: Array[String]): Unit = {
-    val env = StreamExecutionEnvironment.getExecutionEnvironment
+//    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val env = ExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(1)
-//    env.setStateBackend(new RocksDBStateBackend("file:///"))
+    //    env.setStateBackend(new RocksDBStateBackend("file:///"))
     val datasLottery = env.readTextFile("datas/lottery.txt")
-    datasLottery.map(line=>{
-      val strings = line.split(",")
-      strings(2)
+    datasLottery.map(line => {
+      val strings = line.split("[|,]")
+      (strings(strings.length - 1),1)
     })
+      .groupBy(0)
+      .sum(1)
+
       .print()
-    env.execute()
+//    env.execute()
   }
 }
